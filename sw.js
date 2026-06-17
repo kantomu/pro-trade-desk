b42088a68a67f4de5ee9e3ec6f719edd09dffb44
kantomu <matsukan0207@gmail.com>
2026-06-17 18:56:10 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL = "td-shell-v6";
+const SHELL = "td-shell-v7";
 const SHELL_FILES = ["./", "./index.html", "./widget.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
